43959f91dc90d7a83ba48a5baf7241082f6e44e3
olesyamogil <olesya.mogil@gmail.com>
2019-11-20 13:20:37 +0200

    Updates

---
+++ precache-manifest.295a586d21f80494d7f7ba554de028fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f366303ddcf11ab5b8b13d2da8a87a",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "f51dabe23ff0dbb99350",
+    "url": "/react_people-table/static/css/main.509d4b09.chunk.css"
+  },
+  {
+    "revision": "d0261a7fcce5a697c03b",
+    "url": "/react_people-table/static/js/2.f8e1d146.chunk.js"
+  },
+  {
+    "revision": "f51dabe23ff0dbb99350",
+    "url": "/react_people-table/static/js/main.935f2739.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.3d8c6b1e7697442ca25d7e26bf334fd1.js"
+  "/react_people-table/precache-manifest.295a586d21f80494d7f7ba554de028fc.js"
 );
 
 self.addEventListener('message', (event) => {
